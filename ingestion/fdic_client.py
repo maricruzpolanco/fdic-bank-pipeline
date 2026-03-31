1d0cb890eb44f4af72473cd1919857735860727d
Maricruz <7350525+maricruzpolanco@users.noreply.github.com>
2026-03-31 02:35:36 +0000

    WIP: add logging and pagination to fdic_client

--- ingestion/fdic_client.py
+++ ingestion/fdic_client.py
@@ -2,50 +2,86 @@ import requests
 from requests.exceptions import RequestException, HTTPError
 import json
 import logging
+import time
 import pprint as pp
 
 
-base_url = "https://api.fdic.gov/banks/"
+url = "https://api.fdic.gov/banks/"
 
 endpoints = {
     "institutions": {
-        "fields": ["name", "cert", "stalp", "asset", "repdte", "active"],
         "params": {
-            "limit": 500,
+            "fields": "NAME,CERT,STALP,ASSET,REPDTE,ACTIVE",
+            "limit": 10000,
             "offset": 0,
+            "format": "json",
         }
     },
     "financials": {
-        "fields": ["repdte", "cert", "asset", "dep", "lnlsnet", "netinc", "rbcrwaj"],
         "params": {
-            "limit": 500,
+            "fields": "REPDTE,CERT,ASSET,DEP,LNLSNET,NETINC,RBCRWAJ",
+            "limit": 10000,
             "offset": 0,
-            "filter": "REPDTE:[2020-01-01 TO 2024-12-31]"
+            "format": "json",
+            "filters": "REPDTE:[2020-01-01 TO 2024-12-31]"
         }
     },
     "failures": {
-        "fields": ["name", "cert", "faildate", "savr", "restype", "cost"],
         "params": {
-            "limit": 500,
+            "fields": "NAME,CERT,FAILDATE,SAVR,RESTYPE,COST",
+            "limit": 10000,
             "offset": 0,
+            "format": "json"
         }
     }
 }
 
 # pp.pprint(endpoints)
 
+logging.basicConfig(level=logging.INFO)
+
 
 def fetch_fdic_data():
 
     for endpoint, endpoint_info in endpoints.items():
 
-        fields_list = endpoint_info["fields"]
-        fields = ','.join([str(element) for element in fields_list])
-        limit = endpoint_info["params"]["limit"]
-        if endpoint_info["params"]["filter"]:
-            filters = endpoint_info["params"]["filter"]
-        url = f"{base_url}{endpoint}?fields={fields}&limit={limit}&offset=0&format=json&filters"
-        print(url)
+        all_records = []
+
+        while True:
+
+            try:
+
+                response = requests.get(
+                    url + endpoint, params=endpoint_info["params"])
+                logging.info(
+                    f"Sent API request for {endpoint}: {response.url}")
+
+                if response.status_code == 200:
+                    logging.info(f"Request for {endpoint} was successfull")
+                else:
+                    logging.warning(
+                        f"Unexpected status code: {response.status_code}")
+
+                # print(response.base_url)
+                response.raise_for_status()
+                data = response.json()
+                records = data.get("data", [])
+
+                logging.info(
+                    f"Fetched {len(all_records)} records from {endpoint}")
+
+                if not records:
+                    break
+
+                all_records.extend(records)
+                endpoint_info["params"]["offset"] += endpoint_info["params"]["limit"]
+
+                pp.pprint(records)
+
+            except Exception as e:
+                print("An error has occurred with the request:", e)
+
+    return all_records
 
 
 fetch_fdic_data()
