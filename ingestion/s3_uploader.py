e9466a9f52ea75eec3ae0870f7d875534d6e1296
Maricruz <7350525+maricruzpolanco@users.noreply.github.com>
2026-04-03 03:11:36 +0000

    WIP: add local JSON loading logic to s3_uploader

--- ingestion/s3_uploader.py
+++ ingestion/s3_uploader.py
@@ -1,16 +1,28 @@
-from fdic_client import fetch_fdic_data
+# from fdic_client import fetch_fdic_data
 import json
 from datetime import datetime
 import pytz
 
 
-all_data = fetch_fdic_data()
+with open('all_data.json', 'r') as file:
+    all_data = json.load(file)
+
+# all_data = fetch_fdic_data()
+
+records_list = []
 
 
 def s3_uploader(all_data):
     est = pytz.timezone('US/Eastern')
     upload_date = datetime.now(est).date()
     print(upload_date)
+
+    for key, value in all_data.items():
+        records_list.append({"endpoint": key, "fields": value})
+        print(records_list)
+
+    # with open("end.json", "w") as json_file:
+    #     json_dump()
 
 
 s3_uploader(all_data)
